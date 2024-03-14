14991beabe9b6cee7402ba097df6d0d86b56054d
pixelbyaj <abhishek_joshi@epam.com>
2024-03-14 12:03:09 +0100

    Version updated for proper settings and removed unwanted dependencies

--- MXParser/Source/ParsingJsonRules.cs
+++ MXParser/Source/ParsingJsonRules.cs
@@ -1,12 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Net.Http.Json;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace MXParser
 {
--- MXParser/Source/ParsingRules.cs
+++ MXParser/Source/ParsingRules.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlTypes;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using System.Xml;
-using System.Xml.Linq;
-using System.Xml.XPath;
 
 namespace MXParser
 {
